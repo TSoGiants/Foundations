56abfbc9e3360a9c0473d58380aaf2fd2d813356
Steven J. Foland <stevenfoland@tsogiants.org>
2019-06-11 20:30:18 -0500

    End of Episode 3.

---
+++ 2019-2020/MATH_1301_Python/Episode_03/conditionals.py
@@ -0,0 +1,19 @@
+name = input("Please enter your name. > ")
+
+print("Hello, " + name.split(" ")[0] + "!")
+
+x = input("Give me an integer to add: ")
+
+
+try:
+	x = int(x)	
+except:
+	print("The value you entered is not a number.")
+else:
+	y = input("Give me another integer to add: ")
+	try:
+		y = int(y)	
+	except:
+		print("The second value you entered is not a number.")
+	else:
+		print("The sum of your numbers is: " + str(x + y))
