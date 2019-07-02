0c01494ec7340f469037ea1b48a2f0d06851f3fc
Steven J. Foland <stevenfoland@tsogiants.org>
2019-07-02 16:54:02 -0500

    Beginning of Episode 09.

---
+++ 2019-2020/MATH_1301_Python/Episode_09/bubbleSort.py
@@ -0,0 +1,33 @@
+### List Initialization
+
+from numpy import random
+
+LENGTH = 12
+
+list = []
+
+for num in range(LENGTH):
+	list.append(random.randint(100))
+	
+print("Unsorted List:")
+print(list)
+print()
+### Beginning of Bubble Sort Algorithm
+count = 0
+for j in range(len(list)-1, 0, -1):
+	done = True # Maybe we're done?
+	for i in range(0, j):
+		if(list[i+1] < list[i]):
+			# Swap list elements.
+			list[i], list[i+1] = list[i+1], list[i]
+			done = False # I guess we aren't done.
+	count += 1
+	if(done):
+		break
+print("Sorted List:")
+print(list)
+print("Sorted in " + str(count) + " iterations.")
+		
+		
+		
+		
