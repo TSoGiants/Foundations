ef5b3d0182edeeb45d0347878c94e73d14a9a30e
Steven J. Foland <stevenfoland@tsogiants.org>
2019-07-09 20:01:38 -0500

    End of Episode 10 - Hangman half done

--- 2019-2020/MATH_1301_Python/Episode_10/Hangman.py
+++ 2019-2020/MATH_1301_Python/Episode_10/Hangman.py
@@ -1,10 +1,30 @@
+from numpy import random
+import string
+
+letters = string.ascii_lowercase
+
 words = []
 with open("word_list.txt", "r") as f:
 	word_list = f.read()
 	word_list = word_list.split()
-	print(len(word_list))
 	for word in word_list:
 		if len(word) > 3 and len(word) < 9:
 			words.append(word)
-#print(words)
-print(len(words)) # Temporary
+n = random.randint(len(words))
+word = words[n]
+print(word)
+
+guess = input("What is your guess? ").lower()
+if len(guess) == 1 and guess in letters:
+	if guess in word:
+		indices = []
+		for i in range(len(word)):
+			if guess == word[i]:
+				indices.append(i)
+		print("Found your letter at locations: " + str(indices))
+	else:
+		print("not match")
+else:
+	print("not ok")
+	
+
